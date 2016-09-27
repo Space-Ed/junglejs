a268d03253e42beada47763ab812994a5d21d82b
Edward <endalley11@gmail.com>
2016-09-27 15:09:46 +1300

    shaping inventory of builtin functions

--- ts_src/inventory/io.ts
+++ ts_src/inventory/io.ts
@@ -3,8 +3,8 @@ namespace Gentyl. Inventory {
     export function placeInput(input){
         this._placed = input;
     }
-    export function pickupInput(input){
-        return this._placed = input;
+    export function pickupInput(obj, arg){
+        return this._placed;
     }
 
     export function retract(obj, arg){
